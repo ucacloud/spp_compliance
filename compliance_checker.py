d7e0a215c6eb3f117da9952cfb99a829672acadb
ucacloud <mgcnlrar@gmail.com>
2025-07-22 00:24:20 -0500

    added code to count number of each violation

--- compliance_checker.py
+++ compliance_checker.py
@@ -1,4 +1,4 @@
-log_file_path = 'logs/example.log'
+log_file_path = 'logs/example_log.txt'
 
 # Define violation patterns
 violation_patterns = {
@@ -9,15 +9,33 @@ violation_patterns = {
     "SSH Activity": "sshd"
 }
 
+violations = [] #List that stores violation records
+
 with open(log_file_path, 'r') as file:
-    for line in file:
+    for line_number, line in enumerate(file, start=1):
         cleaned_line = line.strip()
         matched = False
 
         for violation_type, pattern in violation_patterns.items():
             if pattern in cleaned_line:
-                print(f" [{violation_type}] {cleaned_line}")
+                violations.append({
+                    "line_number": line_number,
+                    "violation_type": violation_type,
+                    "description": cleaned_line
+                })
                 matched = True
 
         if not matched:
-            print(f" [OK] {cleaned_line}")
+            pass
+for v in violations:
+    print(f"Line {v['line_number']}: [{v['violation_type']}] {v['description']}")
+
+print("\nSummary of Violations:")
+violation_counts = {}
+
+for v in violations:
+    violation_type = v["violation_type"]
+    violation_counts[violation_type] = violation_counts.get(violation_type, 0) + 1
+
+for v_type, count in violation_counts.items():
+    print(f"{v_type}: {count}")
